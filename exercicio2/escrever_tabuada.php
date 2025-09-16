91f67ce9bc7e8099866228c35941523d5beca42b
lielsonml <lielsonml@gmail.com>
2025-09-16 08:44:40 -0300

    .

--- exercicio2/escrever_tabuada.php
+++ exercicio2/escrever_tabuada.php
@@ -1,10 +1,10 @@
 <?php
 $arquivo = fopen("tabuada.txt", "a");
-$num1=$_REQUEST['num1'];
+$num1=(int) $_REQUEST['num1'];
 
 for ($i = 1; $i <= 10; $i++) {
     fwrite($arquivo, "$num1 x $i = " . ($num1 * $i) . "\n");
-    echo "$num1 x $in = " . ($num1 * $i) . "<br>";
+    echo "$num1 x $i = " . ($num1 * $i) . "<br>";
 }
 fclose($arquivo);
 
