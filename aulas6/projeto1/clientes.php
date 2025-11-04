f4cba20c617f293593b9e346fe714e8813de8814
lielsonml <lielsonml@gmail.com>
2025-11-04 14:30:31 -0300

    .

--- aulas6/projeto1/clientes.php
+++ aulas6/projeto1/clientes.php
@@ -1,2 +1,13 @@
 <?php
     require_once 'config.inc.php';
+
+     $sql = "SELECT * FROM clientes";
+    $resultado = mysqli_query($conexao, $sql);
+    while ($dados = mysqli_fetch_array($resultado)) {
+        echo "ID: " . $dados['id'] . " | ";
+        echo "Cliente: " . $dados['cliente'] . " | ";
+        echo "Cidade: " . $dados['cidade'] . " | ";
+        echo "Estado: " . $dados['estado'] . " | ";
+    }
+    mysqli_close($conexao);
+?>
